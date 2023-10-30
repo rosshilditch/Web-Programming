43528824570f6f3f873b59a8941d599890d01a9b
rosshilditch <145106742+rosshilditch@users.noreply.github.com>
2023-10-30 14:30:31 +0000

    Create news.js

---
+++ news.js
@@ -0,0 +1,31 @@
+// Function to fetch and display the news feed
+function fetchNewsFeed() {
+    const rssUrl = 'https://example.com/news/rss'; // Replace with the RSS feed URL of the news website
+
+    fetch(rssUrl)
+        .then(response => response.text())
+        .then(data => {
+            const parser = new DOMParser();
+            const xmlDoc = parser.parseFromString(data, 'text/xml');
+            const items = xmlDoc.querySelectorAll('item');
+
+            const newsList = document.getElementById('news-list');
+            newsList.innerHTML = '';
+
+            items.forEach(item => {
+                const title = item.querySelector('title').textContent;
+                const link = item.querySelector('link').textContent;
+
+                const li = document.createElement('li');
+                const a = document.createElement('a');
+                a.textContent = title;
+                a.href = link;
+                li.appendChild(a);
+                newsList.appendChild(li);
+            });
+        })
+        .catch(error => console.error('Error fetching news feed:', error));
+}
+
+// Fetch and display the news feed when the page loads
+fetchNewsFeed();
